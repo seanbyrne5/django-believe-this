82a3634e28945016481acdfe478e6b85bab63481
Sean Byrne <byrner14@hotmail.com>
2024-07-19 11:29:02 +0000

    add html and css templates

--- blog/models.py
+++ blog/models.py
@@ -25,9 +25,9 @@ class Post(models.Model):
 
 
 class Comment(models.Model):
-    name = models.CharField(max_length=255)
     post = models.ForeignKey(Post, on_delete=models.CASCADE,
                              related_name="comments")
+    name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
